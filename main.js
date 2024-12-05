81f5ec2b198fcfea3397983cf72c293fdf3ec9cc
CHOUAIKH739 <142019970+CHOUAIKH739@users.noreply.github.com>
2024-12-05 11:32:33 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,64 @@
+//change nave style on scroll
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scrolled',window.scrollY > 0);
+})
+//contact button circular text
+const textButtons = document.querySelectorAll('.contact__btn');
+textButtons.forEach(textButtons => {
+    let text = textButtons.querySelector('p');
+    text.innerHTML = text.innerHTML.split('').map((character, index) => `<span style ="transform:rotate(${index * 12}deg)">${character}</span>`).join('')
+})
+
+//swiper js gallery section
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints:{
+        599: {
+            slidesPerView: 2,
+            spaceBetween: 40
+        },
+        1023: {
+            slidesPerView: 3,
+            spaceBetween: 60
+
+        }
+    }
+  });
+  
+  //nav bar open and close
+
+  const nav = document.querySelector('.nav__links');
+  const openNavBtn = document.querySelector('#nav__toggle-open');
+  const closeNavBtn = document.querySelector('#nav__toggle-close');
+
+  const openNav = () => {
+    nav.style.display = 'flex';
+    openNavBtn.style.display = 'none';
+    closeNavBtn.style.display = 'inline-block';
+  }
+
+
+
+
+  openNavBtn.addEventListener('click', openNav);
+
+  const closeNav = () => {
+    nav.style.display = 'none';
+    openNavBtn.style.display = 'inline-block';
+    closeNavBtn.style.display = 'none';
+  }
+  
+
+
+
+  closeNavBtn.addEventListener('click', closeNav);
+
+  nav.querySelectorAll('li a').forEach(navLink => {
+    navLink.addEventListener('click', closeNav);
+  })
+  
